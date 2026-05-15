f47982edcf6df206f4d20a637fcd4b700943ff84
GuiRodriguero <gui.rodriguero@hotmail.com>
2026-05-15 01:13:27 -0300

    Add project exceptions

---
+++ src/main/java/com/gui/jblind/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.gui.jblind.core.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+}
