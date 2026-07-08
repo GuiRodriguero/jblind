74c9d74e76924b472dea51bfe87cadeee0487aaf
GuiRodriguero <gui.rodriguero@hotmail.com>
2026-07-08 02:45:59 -0300

    Create CashGameLog Query and Repository

---
+++ src/test/java/com/gui/jblind/cashgame/CashGameLogQueryTest.java
@@ -0,0 +1,42 @@
+package com.gui.jblind.cashgame;
+
+import com.gui.jblind.TestBase;
+import com.gui.jblind.cashgame.web.CashGameLogResponse;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.when;
+
+class CashGameLogQueryTest extends TestBase {
+
+	private CashGameLogQuery query;
+
+	@Mock
+	private CashGameLogRepository repository;
+
+	@Override
+	public void init() {
+		query = new CashGameLogQuery(repository);
+	}
+
+	@Test
+	void should_find_all_by_cash_game_id() {
+		String cashGameId = valid(String.class);
+		List<CashGameLog> logs = valid(CashGameLog.class, 3);
+
+		when(repository.findAllByCashGameIdOrderByTimestampDesc(cashGameId)).thenReturn(logs);
+
+		assertThat(query.findAllByCashGameId(cashGameId))
+			.isEqualTo(logs.stream().map(CashGameLogResponse::from).toList());
+
+		InOrder inOrder = inOrder(repository);
+		inOrder.verify(repository).findAllByCashGameIdOrderByTimestampDesc(cashGameId);
+		inOrder.verifyNoMoreInteractions();
+	}
+
+}
